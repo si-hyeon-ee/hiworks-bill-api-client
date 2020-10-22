d125dcadbfa78f33fdf17caf7b9e5d8308364a26
isaac <si.hyeon.chan@gmail.com>
2020-10-22 21:59:34 +0900

    information에 대한 검증 룰 추가

--- src/dto/information.dto.ts
+++ src/dto/information.dto.ts
@@ -1,14 +1,58 @@
+import { Transform } from "class-transformer";
+import { IsNotEmpty, IsNumber, IsOptional, IsString,Matches } from "class-validator";
+
 export class Information {
+    
+    @IsNotEmpty()
+    @Matches(/\d{4}-\d{2}-\d{2}/)
+    @IsString()
     issueDate!:string
+
+    @IsString()
+    @IsOptional()
     memo?:string
+
+    @IsString()
+    @IsOptional()
     bookNo?:string
+
+    @IsString()
+    @IsOptional()
     serial?:string
+
+    @IsString()
+    @IsOptional()
     description?:string
+
+    @Transform(value=>Number(value))
+    @IsNumber()
+    @IsOptional()
     cash?:number
+
+    @Transform(value=>Number(value))
+    @IsNumber()
+    @IsOptional()
     check?:number
+
+    @Transform(value=>Number(value))
+    @IsNumber()
+    @IsOptional()
     draft?:number
+
+    @Transform(value=>Number(value))
+    @IsNumber()
+    @IsOptional()
     uncollected?:number
+
+    @Matches(/\d{18}|-\d{17}/)
+    @IsString()
+    @IsNotEmpty()
     totalSupplyPrice!:string
+
+    @Matches(/\d{18}|-\d{17}/)
+    @IsString()
+    @IsNotEmpty()
     totalTax!:string
+    
     constructor(part?:Partial<Information>) { if(part) Object.assign(this, part) }
 }
